286d11b0828fc3bc11a6ef5bfac4092ee743e0ee
Katie Bickford <katie.bickford@thedatalab.org>
2024-10-25 14:48:34 +0100

    Update URL + views for adding to existing lists
     FT passes :-)

--- lists/tests.py
+++ lists/tests.py
@@ -57,6 +57,12 @@ class ListViewTest(TestCase):
         self.assertContains(response, "itemey 1")
         self.assertContains(response, "itemey 2")
         self.assertNotContains(response, "other list item")
+    
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get(f"/lists/{correct_list.id}/")
+        self.assertEqual(response.context["list"], correct_list)
 
 class NewListTest(TestCase):
     def test_can_save_a_post_request(self):
