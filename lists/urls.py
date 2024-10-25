20bcdde83c474c19275551ec95fd684d665eb9ec
Katie Bickford <katie.bickford@thedatalab.org>
2024-10-25 14:55:11 +0100

    Refactor urls
    Django encourages use of separate app/urls.py files, to make apps more self-contained. This was not how the project was originally set up, so some refactoring was necassary to achieve this.

--- lists/urls.py
+++ lists/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import include, path
-from lists import views as list_views
+from django.contrib import admin
+from django.urls import path
+from lists.views import home_page, view_list, new_list, add_item
 
 urlpatterns = [
-    path("", list_views.home_page, name="home"),
-    path("lists/", include("lists.urls")),
+    path('admin/', admin.site.urls),
+    path("", home_page, name="home"),
+    path("new", new_list, name="new_list"),
+    path("<int:list_id>/", view_list, name="view_list"),
+    path("<int:list_id>/add_item", add_item, name="add_item"),
 ]
