d061667ff83c2f75e56f1059b3571088e4018c2d
Katie Bickford <katie.bickford@thedatalab.org>
2024-12-18 17:53:12 +0000

    Refactor list view to handle new item POSTs

--- src/lists/urls.py
+++ src/lists/urls.py
@@ -16,12 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from lists.views import home_page, view_list, new_list, add_item
+from lists.views import home_page, view_list, new_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", home_page, name="home"),
     path("new", new_list, name="new_list"),
     path("<int:list_id>/", view_list, name="view_list"),
-    path("<int:list_id>/add_item", add_item, name="add_item"),
 ]
