9fdd4f23ceb4e18385351e9dad8caae4533cca6a
Katie Bickford <katie.bickford@thedatalab.org>
2025-04-03 12:32:55 +0100

    Add new my_lists view, template, URL pattern and unit test

--- src/lists/urls.py
+++ src/lists/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from lists.views import home_page, view_list, new_list
+from lists.views import home_page, view_list, new_list, my_lists
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", home_page, name="home"),
     path("new", new_list, name="new_list"),
     path("<int:list_id>/", view_list, name="view_list"),
+    path("users/<str:email>/", my_lists, name="my_lists"),
 ]
